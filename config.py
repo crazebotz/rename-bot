ee3696f715252e0f56646f26b63ce84d901ac510
crazebotz <120011436+crazebotz@users.noreply.github.com>
2023-02-06 09:23:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,12 @@ class Config(object):
    
     
     # your telegram id
-    ADMIN = int(os.environ.get("ADMIN", ""))
+    ADMIN = set(str(x) for x in os.environ.get("ADMIN", "").split())
     CHANNEL = int(os.environ.get("CHANNEL", "-1001332181134"))
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
 
     # database uri (mongodb)
     DB_URL = os.environ.get("DB_URL", "")
-    DB_NAME = os.environ.get("MYDATABASE", "")
+    DB_NAME = os.environ.get("MYDATABASE", "MYDB")
     STRING = (os.environ.get("STRING", ""))
  
