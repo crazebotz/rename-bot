7bda2cec376886c7ff6f026d9dce1a7224c2b930
crazebotz <120011436+crazebotz@users.noreply.github.com>
2023-02-06 10:15:33 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,5 +1,6 @@
 import os
-import config
+from config import Config
+
 from pyrogram import Client, filters
 from helper.date import add_date
 from helper.database import uploadlimit , usertype,addpre
